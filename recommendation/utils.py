a862f40dabe36d295ca9f9f80cc4346f2b5cd3c4
ddori <ddorigod@gmail.com>
2018-10-03 20:56:06 +0900

    csv파일 db에 저장하는 함수 추가

--- recommendation/utils.py
+++ recommendation/utils.py
@@ -1,9 +1,34 @@
+import csv
 import time
 import datetime
+import pytz
+
+from .models import ReviewModel
 
 
 def time_to_datetime(time_data: int):
     t = time.gmtime(time_data)
     time_tuple = (t.tm_year, t.tm_mon, t.tm_hour)
 
-    return datetime.datetime(*time_tuple, 0, 0)
+    return datetime.datetime(*time_tuple, 0, 0, tzinfo=pytz.UTC)
+
+
+def save_data(*args):
+    user_id = int(args[0])
+    trip_id = int(args[1])
+    rating = float(args[2])
+    timestamp = time_to_datetime(int(args[3]))
+
+    ReviewModel(user_id=user_id, trip_id=trip_id, rating=rating, timestamp=timestamp).save()
+
+
+def import_data(file_name):
+    f = open(file_name, 'r', encoding='utf-8')
+    rdr = csv.reader(f)
+    for line in rdr:
+        save_data(*line)
+    f.close()
+
+
+
+
