4375315025e3c5de820c8b9a8d3cb02d5e5621aa
ddori <ddorigod@gmail.com>
2018-10-03 22:01:02 +0900

    추천 시스템 view 추가

--- recommendation/views.py
+++ recommendation/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_GET
+from .ml import main_project
 
-d
+
+@require_GET
+@csrf_exempt
+def recommend_view(user_id):
+    return main_project(user_id)
